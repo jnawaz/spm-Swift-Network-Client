c4508daff8728878e5d39e9ce320436fa141355a
Jamil Nawaz <jamilnawaz88@gmail.com>
2023-10-31 20:22:58 +0000

    Testing internal visibility

---
+++ Sources/NetworkClient/Internal.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jamil Nawaz on 31/10/2023.
+//
+
+import Foundation
+
+internal class Internal {
+    func doSomething() {
+        print("nowts happening")
+    }
+}
